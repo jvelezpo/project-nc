bc74f387d0121cc9b9f66c106f9d4ce1339bc9fb
miltoncodeyt <miltoncodeyt@gmail.com>
2019-12-19 18:45:49 -0500

    Header add

--- src/components/images/medellin-banner.js
+++ src/components/images/medellin-banner.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "medellin.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "medellin_banner.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1200) {
             ...GatsbyImageSharpFluid
