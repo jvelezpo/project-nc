3bdbeacfd140c40f078d008c9383d1f3b3b576a3
jvelezpo <sebasvelez@gmail.com>
2019-12-19 15:12:52 -0500

    venue

--- src/components/Venue/Venue.js
+++ src/components/Venue/Venue.js
@@ -1,10 +1,23 @@
 import React from "react"
 
+import RutaN from '../images/ruta-n'
+
 import "./Venue.scss"
 
 const Venue = () => (
   <div className="venue-component venue">
-    Venue
+    <div className="rutan-image">
+      <RutaN />
+    </div>
+    <div className="content">
+      <div className="title">
+        Venue
+      </div>
+      <div>
+        consectetur adipiscing elit. Vivamus lacinia quis arcu non elementum. Phasellus vestibulum rutrum tincidunt. Maecenas vitae ex turpis. consectetur adipiscing elit. Vivamus lacinia quis arcu non elementum. Phasellus vestibulum rutrum tincidunt. Maecenas vitae ex turpis.
+      </div>
+      <button className="reservation">Reservation</button>
+    </div>
   </div>
 )
 
