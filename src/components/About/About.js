168e2e5b47a2248056c963f4433d2fb22f9ef79c
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2019-12-20 17:48:52 -0500

    correción about y modulos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ const About = () => (
     <div className="title">About</div>
     <div className="description">
       <div className="content">
-        NodeConf Colombia 2019 is the first international event focused on the
+        NodeConf Colombia 2020 is the first international event focused on the
         entire Node.js ecosystem. It’s a non-profit event, where our attendees
         will be sharing in an environment of inclusion and respect, having
         access to relevant information through talks, workshops, and great
