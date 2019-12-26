42945fd6ded02b41ae348c32aa3246350c31ebde
miltoncodeyt <miltoncodeyt@gmail.com>
2019-12-26 09:25:45 -0500

    correction CFP

--- src/components/CFP/CFP.js
+++ src/components/CFP/CFP.js
@@ -3,21 +3,17 @@ import React from "react"
 import "./CFP.scss"
 
 const CFP = () => (
-  <section className="announcing-container  l-container  l-inner-p" id="cfp">
-    <div className="section-announcing">
-      <h2 className="section-announcing__title  section__title">
-        call for speakers
-      </h2>
-      <p className="section-announcing__subtitle">Got a talk idea? We want to see it!</p> 
-      <p className="section-announcing__description">
-         We want you to be part of NodeConf
-        Colombia 2020; We are the first exclusive Node.js conference in
-        Colombia, where community members from around the world come together to
-        learn and share information about the latest trends and technologies in
-        Node.js development. Apply now. This call for presenters closes at 11:59
-        PM EST on February 28TH, 2020.
-      </p>
-    </div>
+  <section className="section-cfp  l-container  l-inner-p" id="cfp">
+    <h2 className="section-cfp__title  section__title">call for speakers</h2>
+    <p className="section-cfp__subtitle">Got a talk idea? We want to see it!</p> 
+    <p className="section-cfp__description">
+        We want you to be part of NodeConf
+      Colombia 2020; We are the first exclusive Node.js conference in
+      Colombia, where community members from around the world come together to
+      learn and share information about the latest trends and technologies in
+      Node.js development. Apply now. This call for presenters closes at 11:59
+      PM EST on February 28TH, 2020.
+    </p>
   </section>
 )
 
