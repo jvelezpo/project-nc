da6a5eb35a4785ade9beee3ffc8c63e2daf5c7a1
miltoncodeyt <miltoncodeyt@gmail.com>
2019-12-26 11:40:17 -0500

    corrections About & Team

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,19 +3,16 @@ import React from "react"
 import "./About.scss"
 
 const About = () => (
-  <div className="about-component about" id="about">
-    <div className="triangle" />
-    <div className="title">About</div>
-    <div className="description">
-      <div className="content">
-        NodeConf Colombia 2020 is the first international event focused on the
-        entire Node.js ecosystem. It’s a non-profit event, where our attendees
-        will be sharing in an environment of inclusion and respect, having
-        access to relevant information through talks, workshops, and great
-        experiences with the Colombian Node community.
-      </div>
-    </div>
-  </div>
+  <section className="about-component about  l-inner-p" id="about">
+    <h2 className="title  section__title">about</h2>
+    <p className="description section__description">
+      NodeConf Colombia 2020 is the first international event focused on the
+      entire Node.js ecosystem. It’s a non-profit event, where our attendees
+      will be sharing in an environment of inclusion and respect, having
+      access to relevant information through talks, workshops, and great
+      experiences with the Colombian Node community.
+    </p>
+  </section>
 )
 
 export default About
