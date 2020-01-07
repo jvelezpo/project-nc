2b390f74cd51a6204bb2067ae212ae7a7a888fcc
jvelezpo <sebasvelez@gmail.com>
2020-01-06 19:54:46 -0500

    set theme_color to the conf green

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#2d9983`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
